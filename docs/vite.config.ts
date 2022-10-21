8e1bf7cd2196e25350fbe5ae57381c9a753ec415
jhinzhou <windbell520gxq@gmail.com>
2022-10-21 15:35:08 +0800

    feat: upgrade dependencies

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import Unocss from "../config/unocss";
-// https://vitejs.dev/config/
-
 export default defineConfig({
-    plugins: [
-        // 添加JSX插件
-        vueJsx(),
-        Unocss(),
-    ],
+  plugins: [
+    // 添加JSX插件
+    vueJsx(),
+    Unocss(),
+  ],
 });
