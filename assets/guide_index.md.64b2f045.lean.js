e46edd83cc4909166df2c32dd30715f64a641e30
jhinzhou <jhinzhou@users.noreply.github.com>
2023-03-28 03:22:47 +0000

    deploy: 0eafc3916fdf1d2b5ed91c43157523c3ba1bd30b

---
+++ assets/guide_index.md.64b2f045.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as t,N as i}from"./chunks/framework.6a47aca6.js";const m=JSON.parse('{"title":"快速开始","description":"","frontmatter":{},"headers":[],"relativePath":"guide/index.md"}'),c={name:"guide/index.md"},a=i("",2),d=[a];function n(l,_,s,r,p,h){return t(),o("div",null,d)}const f=e(c,[["render",n]]);export{m as __pageData,f as default};
